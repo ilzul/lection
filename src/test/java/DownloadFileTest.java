a5699c88d5d594316fd91da68e14feb6bd406f3a
Ilya Zhulanov <ilzul@hotmail.com>
2015-10-14 19:19:22 +0300

    Minor corrections

--- src/test/java/DownloadFileTest.java
+++ src/test/java/DownloadFileTest.java
@@ -1,5 +1,7 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Paths;
@@ -7,6 +9,15 @@ import java.nio.file.Paths;
 import static org.junit.Assert.assertTrue;
 
 public class DownloadFileTest {
+
+    @Before
+    public void setUp(){
+        try {
+            Files.deleteIfExists(Paths.get("NewTxt.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void verifyThatFileWasDownloaded() throws Exception {
